292c0315b2458c1f0f95638897ca84b5edab22f1
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2024-10-07 10:52:43 -0400

    10/7/24
    Not fully functional, need to set the coordinate points inputted by the user as the same points the user uses to calc slope and everything else.

--- EquationRunner.java
+++ EquationRunner.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class EquationRunner {
+
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        LinearEquation le = new LinearEquation(5,2,5,6);
+        System.out.print(le.toString());
 
-    LinearEquation linearEquation = new LinearEquation(3, 2, 1, 5);
-    linearEquation.toString();
 
 
 
-        }
+    }
 }
--- LinearEquation.java
+++ LinearEquation.java
@@ -6,9 +6,9 @@ public class LinearEquation {
     private int x2;
     private int y2;
     private double slope;
-    private double distance;
     private double y_intercept;
     private double coordinate_point;
+    private double distance;
 
     public LinearEquation(int x, int y, int x2, int y2) {
         this.x = x;
@@ -16,18 +16,20 @@ public class LinearEquation {
         this.x2 = x2;
         this.y2 =y2;
 
+
     }
 
-    public void set_xValue(int new_xValue) {
+    public void set_xValue(int new_xValue, int new_yValue) {
         x = new_xValue;
-
-    }
-
-    public void set_yValue(int new_yValue) {
         y = new_yValue;
 
     }
 
+    public void set_yValue(int new_x2Value, int new_y2Value) {
+        x2 = new_x2Value;
+        y2 = new_y2Value;
+
+    }
 
     public double slope() {
         slope = Math.sqrt(Math.pow((y2 - y), 2) / Math.pow((x2 - x), 2));
@@ -39,7 +41,6 @@ public class LinearEquation {
         y_intercept = -((slope * x) - y);
         return y_intercept;
     }
-
 
 
     public double distance() {
@@ -55,25 +56,26 @@ public class LinearEquation {
 
     public String toString() {
         Scanner s = new Scanner(System.in);
-        String str =  "Enter your first Coordinate Point: ";
+        System.out.print("Enter your first Coordinate Point: ");
         String x = s.nextLine();
 
-        str += "\n" + "Enter your second Coordinate Point: ";
+        System.out.print("\n" + "Enter your second Coordinate Point: ");
         String y = s.nextLine();
 
-        str += "\n" + "First pair: " + x + "\n";
-        str += "Second pair: " + y + "\n";
-        str += "Slope of line: " + slope + "\n";
-        str += "Y-intercept: " + y_intercept + "\n";
-        str += "Slope intercept form: " + "\n";
-        str += "Distance between points: " + distance + "\n";
+        System.out.print("\n" + "First pair: " + set_xValue() + "\n");
+        System.out.print("Second pair: " + set_yValue() + "\n");
+        System.out.print("Slope of line: " + slope() + "\n");
+        System.out.print("Y-intercept: " + y_intercept() + "\n");
+        System.out.print("Slope intercept form: " + slope + "x +" + y_intercept + "\n");
+        System.out.print("Distance between points: " + distance() + "\n");
 
-        str +=  "Enter a third x-value: " + "\n";
+        System.out.print("Enter a third x-value: " );
         String x3 = s.nextLine();
 
-        str += "Solved coordinate point is: " + "(" + x3 + "," + coordinate_point + ")";
+        System.out.print("Solved coordinate point is: " + "(" + x3 + "," + getCoordinate_point() + ")");
 
-        return str;
+
+        return toString();
 
     }
 
